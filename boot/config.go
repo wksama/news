1fbd7913c414be594c792993c9f9b4f3f3d80f99
Wenke Chen <1131691313@qq.com>
2021-12-26 18:11:50 +0800

    Update

--- boot/config.go
+++ boot/config.go
@@ -10,11 +10,11 @@ import (
 )
 
 func initConfig() {
-	viper.SetConfigName("config")   // name of config file (without extension)
-	viper.SetConfigType("yaml")     // REQUIRED if the config file does not have the extension in the name
-	viper.AddConfigPath("./config") // path to look for the config file in
-	err := viper.ReadInConfig()     // Find and read the config file
-	if err != nil {                 // Handle errors reading the config file
+	viper.SetConfigName("config") // name of config file (without extension)
+	viper.SetConfigType("yaml")   // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath(".")      // path to look for the config file in
+	err := viper.ReadInConfig()   // Find and read the config file
+	if err != nil {               // Handle errors reading the config file
 		panic(fmt.Errorf("Fatal error config file: %s \n", err))
 	}
 
