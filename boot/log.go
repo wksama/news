21a44317c81a01576f4875b27af562f86da88f45
Wenke Chen <1131691313@qq.com>
2021-12-28 21:36:47 +0800

    Update log config

--- boot/log.go
+++ boot/log.go
@@ -1,15 +1,26 @@
 package boot
 
 import (
+	"github.com/spf13/viper"
+	"io"
 	"log"
+	"os"
 )
 
 func initLog() {
-	//f, err := os.OpenFile(viper.GetString("log.file"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	//if err != nil {
-	//	panic(errors.Errorf("error opening log file: %v", err))
-	//}
+	var f io.Writer
+	f = os.Stdout
+	logFile := viper.GetString("log.file")
+	if logFile != "" {
+		if _, err := os.Stat(logFile); err != nil {
+			f, err = os.Create(logFile)
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+
 	log.SetFlags(log.Ldate | log.Ltime | log.Llongfile)
 
-	//log.SetOutput(f)
+	log.SetOutput(f)
 }
