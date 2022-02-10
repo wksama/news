032daba0d971c3c058919e02902bfcb9e84ec1ad
Wenke Chen <wenke.chen@mdpi.com>
2022-02-10 14:00:15 +0800

    Update sitemap

--- service/sitemap/sitemap.go
+++ service/sitemap/sitemap.go
@@ -3,6 +3,7 @@ package sitemap
 import (
 	"encoding/xml"
 	"fmt"
+	"github.com/spf13/viper"
 	"news/service/tpl"
 	"news/utils"
 	"os"
@@ -12,34 +13,40 @@ type Sitemap struct {
 	XMLName xml.Name `xml:"urlset"`
 	Xmlns   string   `xml:"xmlns,attr"`
 
-	Links []Url `xml:"url"`
+	Links []Link `xml:"url"`
 }
 
-type Url struct {
-	Loc        string  `xml:"loc"`
-	LastMod    string  `xml:"lastmod"`
-	ChangeFreq string  `xml:"changefreq"`
-	Priority   float64 `xml:"priority"`
+type Link struct {
+	Loc string `xml:"loc"`
+	//LastMod    string  `xml:"lastmod"`
+	//ChangeFreq string  `xml:"changefreq"`
+	Priority float64 `xml:"priority"`
 }
 
-func ListItem2Link(item utils.ListItem) Url {
+func ListItem2Link(item utils.ListItem) Link {
 	dateStr := fmt.Sprintf("%.0f", item.Score)
-	return Url{
-		Loc:        tpl.GetUrlByDateStr(dateStr),
-		LastMod:    dateStr,
-		ChangeFreq: "monthly",
-		Priority:   0.8,
+	return Link{
+		Loc: tpl.GetUrlByDateStr(dateStr),
+		//LastMod:  dateStr,
+		Priority: 0.8,
 	}
 }
 
 func New() *Sitemap {
-	return &Sitemap{
+	sitemap := &Sitemap{
 		Xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9",
-		Links: make([]Url, 0),
+		Links: make([]Link, 0),
 	}
+
+	sitemap.Links = append(sitemap.Links, Link{
+		Loc:      viper.GetString("app.baseUrl"),
+		Priority: 1.0,
+	})
+
+	return sitemap
 }
 
-func (s *Sitemap) Add(link Url) {
+func (s *Sitemap) Add(link Link) {
 	s.Links = append(s.Links, link)
 }
 
